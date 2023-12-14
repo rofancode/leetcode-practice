d3381009ca9d28b78a83970867dcfdd89b538936
rofancode <75173954+rofancode@users.noreply.github.com>
2023-12-15 01:19:41 +0900

    Time: 1 ms, Memory: 44.5 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,20 +1,10 @@
-import java.util.*;
 
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        
         int result = 0;
-        for (int num : map.keySet()) {
-            if (map.get(num) == 1) {
-                result = num;
-                break;
-            }
+        for(int num : nums) {
+            result ^= num;
         }
-        
-        return result ;
+        return result;
     }
 }
