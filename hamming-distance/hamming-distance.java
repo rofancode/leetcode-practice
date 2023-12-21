e391290c417225f9a5b2812aa295b53e795f47ef
rofancode <75173954+rofancode@users.noreply.github.com>
2023-12-22 02:50:57 +0900

    Time: 0 ms, Memory: 40.2 MB - LeetHub

---
+++ hamming-distance/hamming-distance.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        //비트 서로 다름 1 * 0 = 1, 1*1 = 0, 0*0 = 0 ^
+        int xor = x^y;
+        int distance = 0;
+        while(xor != 0) {
+            if(xor % 2 == 1) //홀수인경우
+                distance++;
+            xor = xor >> 1;
+        }
+        
+        return distance;
+    }
+}
