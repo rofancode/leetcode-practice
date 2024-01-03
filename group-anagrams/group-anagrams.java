0f1d6e3927a2d39da4e8a0769af22d4fa1c56b5e
rofancode <75173954+rofancode@users.noreply.github.com>
2024-01-04 00:51:10 +0900

    Time: 910 ms, Memory: 48.1 MB - LeetHub

---
+++ group-anagrams/group-anagrams.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        
+        
+        for (String str : strs) {
+            boolean hasAnagram = false;
+            
+            //anagrams 확인
+            for (List<String> anagram: ans) {
+                hasAnagram = isAnagram(str, anagram.get(0));
+                if(hasAnagram){
+                    anagram.add(str);
+                    break;
+                }
+            }
+            // 없으면 추가
+            if(!hasAnagram) {
+                List<String> anagram = new ArrayList<>();
+                anagram.add(str);
+                ans.add(anagram);
+            }
+        } 
+        
+        return ans;
+        
+    }
+    
+    public boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        
+        int [] alph = new int [26];
+        for (int i = 0; i < str1.length(); i++) {
+            alph[str1.charAt(i) -'a']++;
+            alph[str2.charAt(i) -'a']--;
+        }
+        
+        for (int i : alph){
+            if (i != 0) return false;
+        }
+        
+        return true;
+    }
+}
