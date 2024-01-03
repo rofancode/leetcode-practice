899c8c186de0190d214d9cbcee5b2bc95deb2b00
rofancode <75173954+rofancode@users.noreply.github.com>
2024-01-04 00:34:48 +0900

    Time: 0 ms, Memory: 45.5 MB - LeetHub

--- 0set-matrix-zeroes/0set-matrix-zeroes.java
+++ 0set-matrix-zeroes/0set-matrix-zeroes.java
@@ -1,31 +1,50 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //  O(MN)
+        //  Time O(MN), Space O(M+N)
         /*
             전체 훓으면서 0일 때 i, j 값 저장 
-            
+            첫번째 열, 컬럼을 flag로 사용
+            이때 0,0 의 경우 첫번째 열 혹은 컬럼에 0 이 있을 때 이 0이 컬럼의 0인지 열의 0인지 구분이 어렵다 
+            -> 첫번째 열 or 컬럼 1개 기준으로 셀 확인해서 flag설정 필요
+            -> 0.0은 열의 0으로 설정
+            -> 컬럼의 0은 isCall flag로 뺌 
         */
         int M = matrix.length;
         int N = matrix[0].length;
-        boolean [] rowZero = new boolean[M];
-        boolean [] colZero = new boolean[N];
+        
+        boolean isCol = false;
+        
         for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j ++) {
-                if (matrix[i][j] != 0)
-                    continue;
-                
-                if (!rowZero[i]) 
-                    rowZero[i] = true;
-                if (!colZero[j])
-                    colZero[j] = true;
+            if (matrix[i][0] == 0)
+                isCol = true;
+            //[i][0] == 0 일때 0,0 = 0 세팅 방지
+            for (int j = 1; j < N; j ++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        // 첫번째 열, 첫번째 컬럼 제외하고 내부 먼저 update
+        for (int i = 1; i < M; i++) {
+            for (int j = 1; j < N; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) 
+                    matrix[i][j] = 0;
             }
         }
         
-         for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (rowZero[i] || colZero[j]) 
-                    matrix[i][j] = 0;
+        // 첫번째 열 update
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < N; j ++) {
+                matrix[0][j] = 0;
             }
-         }
+        }
+        
+        // 첫번째 컬럼 update
+        if (isCol) {
+            for (int i = 0; i < M; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
