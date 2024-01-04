bfa8d8193aa529889e79c700314fa0eab30b25c2
rofancode <75173954+rofancode@users.noreply.github.com>
2024-01-05 00:35:02 +0900

    Time: 0 ms, Memory: 44.7 MB - LeetHub

--- 0odd-even-linked-list/0odd-even-linked-list.java
+++ 0odd-even-linked-list/0odd-even-linked-list.java
@@ -10,36 +10,23 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        // odd 일 때 next = next
-        // even 따로 저장
-        // even 으로 끝남 -> next.next = null
-        // odd로 끝남 -> next  = null -> 연결
-        if ( head == null || head.next == null) return head;
         
-        ListNode dummyHead = new ListNode(0, head);
-        ListNode curr = dummyHead.next;
-        ListNode evenHead = new ListNode(0);
-        ListNode even = evenHead;
-        while(curr.next != null && curr.next.next != null){// 앞에 2개 남아있어야함
-            //even 처리
-            even.next = curr.next;
-            curr.next = curr.next.next;
-            curr = curr.next;
+        if ( head == null) return head;
+        
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        
+        while(odd.next != null && odd.next.next != null){// 앞에 2개 남아있어야함
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            
+            odd = odd.next;
             even = even.next;
         }
         
-
-        
-        // odd로 끝난경우 curr.next = null
-        // curr.next 는 있고 curr.next.next = null
-        if (curr.next != null) {
-            even.next = curr.next;
-            even = even.next;
-        }
-        even.next = null;
-        curr.next = evenHead.next;
-        
-        
-        return dummyHead.next;
+        odd.next = evenHead;
+           
+        return head;
     }
 }
